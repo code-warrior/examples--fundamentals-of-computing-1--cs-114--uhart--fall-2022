261d47990a6751f93a45808d829221763a256a87
Roy A Vanegas <roy@thecodeeducators.com>
2022-09-07 08:32:51 -0400

    Add comments example

---
+++ in-class-examples/7-sept-2022/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    // Here is a single-line comment that really
+    // shouldn’t span more than one line, but it does!
+
+    /*
+     * Here is a true, multi-line comment that VS Code
+     * helps with, because, when I hit the return key,
+     * VS Code precedes the line with an asterisk.
+     */
+
+     /**
+      * Here is a javadoc comment, which, in essence is
+      * a multi-line comment with some extras.
+      * @param args
+      */
+    public static void main(String args[]) {
+        System.out.println("Working with comments");
+    }
+}
