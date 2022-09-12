6d3ccebe907ea814a909f9f6bf9ad8bf2501d42a
Roy Vanegas <roy@thecodeeducators.com>
2022-09-12 09:45:25 -0400

    Add today’s examples

---
+++ in-class-examples/12-sept-2022/numeric-types/WorkingWithNumbers.java
@@ -0,0 +1,17 @@
+public class WorkingWithNumbers {
+    public static void main(String args[]) {
+        int a = 10;
+
+        // The maximum length of a password must be 30
+        final int MAX = 48;
+
+        System.out.println("The maximum password " +
+            "you may enter is " +
+                MAX_PASSWORD);
+
+        System.out.println(a++); // Prints 10, increments to 11
+        System.out.println(a++); // Prints 11, increments to 12
+        System.out.println(++a); // Increment to 13, then print
+        System.out.println(a);   // Simply print
+    }
+}
