396b997f416c469f519e0c17c053912aaccffcd2
Roy Vanegas <roy@thecodeeducators.com>
2022-09-19 09:16:40 -0400

    Add simple if statement example

---
+++ in-class-examples/the-if-statement/TheIfStatement.java
@@ -0,0 +1,11 @@
+public class TheIfStatement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        if ((x == 10) && (y == 20)) {
+            System.out.println("\tX4 is equal to 10 and y is equal to 20");
+            System.out.println("\tThe final value of y is " + y);
+        }
+    }
+}
