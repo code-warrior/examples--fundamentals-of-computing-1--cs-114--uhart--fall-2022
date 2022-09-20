82d20ca4a6d9c2f5b33dee88273105de971134f5
Roy A Vanegas <roy@thecodeeducators.com>
2022-09-20 10:45:09 -0400

    Update example of the if statement

--- in-class-examples/the-if-statement/TheIfStatement.java
+++ in-class-examples/the-if-statement/TheIfStatement.java
@@ -3,9 +3,26 @@ public class TheIfStatement {
         int x = 10;
         int y = 20;
 
-        if ((x == 10) && (y == 20)) {
-            System.out.println("\tX4 is equal to 10 and y is equal to 20");
-            System.out.println("\tThe final value of y is " + y);
+        /*
+         * Because x is not equal to 9, the expression on the right is never
+         * evaluated, because of the short-circuited nature of the Boolean &&.
+         * Consequently, the post-fix operator never increments y.
+         */
+        if ((x == 9) && (y++ == 20)) {
+            System.out.println("\n\tx is equal to 10 and y is equal to 20");
         }
+
+        System.out.println("\n\tThe current value of y is " + y + "\n");
+
+        /*
+         * The following Boolean && forces evaluation of the right hand
+         * expression, because the left hand expression (x++ == 10) yields true.
+         */
+        if ((x++ == 10) && (y++ == 20)) {
+            System.out.println("\tx is equal to 10 and y is equal to 20\n");
+        }
+
+        System.out.println("\tThe current value of x is " + x +
+            " and the current value of y is " + y);
     }
 }
