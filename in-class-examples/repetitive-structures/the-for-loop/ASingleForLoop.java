588c5afd665ee3a5153ef4126956d75f7d3f0031
Roy Vanegas <roy@thecodeeducators.com>
2022-09-21 09:37:31 -0400

    Add examples of a single for loop

---
+++ in-class-examples/repetitive-structures/the-for-loop/ASingleForLoop.java
@@ -0,0 +1,38 @@
+public class ASingleForLoop {
+    public static void main(String[] args) {
+        // for(int i = 0; i < 100; i += 50) {
+        //     System.out.println("The value of i is " + i);
+        // }
+
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                System.out.println("i is " + i + " and j is " + j);
+            }
+        }
+
+        // for(int i = 0; i < 10; ) {
+        //     System.out.println("The value of i is " + i);
+
+        //     i++;
+        // }
+
+
+        // int i = 0;
+
+        // for( ;i < 10; ) {
+        //     System.out.println("The value of i is " + i);
+
+        //     i++;
+        // }
+
+
+        // int i = 0;
+
+        // for( ; i < 10; i++) {
+        //     System.out.println("The value of i is " + i);
+        // }
+
+        // for( ; ; )
+        //     ;
+    }
+}
