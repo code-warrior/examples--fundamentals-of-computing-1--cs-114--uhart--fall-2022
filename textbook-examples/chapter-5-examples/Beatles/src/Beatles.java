bc1fd9ab47926c6f893bdd0843875d325b33b6ba
Roy A Vanegas <roy@thecodeeducators.com>
2022-10-04 13:20:26 -0400

    format example

--- textbook-examples/chapter-5-examples/Beatles/src/Beatles.java
+++ textbook-examples/chapter-5-examples/Beatles/src/Beatles.java
@@ -1,18 +1,16 @@
-//********************************************************************
+// *****************************************************************************
 //  Beatles.java       Author: Lewis/Loftus
 //
 //  Demonstrates the use of a ArrayList object.
-//********************************************************************
+// *****************************************************************************
 
 import java.util.ArrayList;
 
-public class Beatles
-{
-    //-----------------------------------------------------------------
+public class Beatles {
+    //--------------------------------------------------------------------------
     //  Stores and modifies a list of band members.
-    //-----------------------------------------------------------------
-    public static void main(String[] args)
-    {
+    //--------------------------------------------------------------------------
+    public static void main(String[] args) {
         ArrayList<String> band = new ArrayList<String>();
 
         band.add("Paul");
@@ -22,6 +20,7 @@ public class Beatles
 
         System.out.println(band);
         int location = band.indexOf("Pete");
+
         band.remove(location);
 
         System.out.println(band);
@@ -30,8 +29,8 @@ public class Beatles
 
         System.out.println("Size of the band: " + band.size());
         int index = 0;
-        while (index < band.size())
-        {
+
+        while (index < band.size()) {
             System.out.println(band.get(index));
             index++;
         }
