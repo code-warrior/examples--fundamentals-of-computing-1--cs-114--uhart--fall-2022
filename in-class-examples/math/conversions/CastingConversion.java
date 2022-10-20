d7a2efb0c9a48231fc0307c414f88222319c11dd
Roy A Vanegas <roy@thecodeeducators.com>
2022-10-20 10:35:02 -0400

    Update the CastingConversion example

--- in-class-examples/math/conversions/CastingConversion.java
+++ in-class-examples/math/conversions/CastingConversion.java
@@ -1,12 +1,19 @@
 public class CastingConversion {
     public static void main(String[] args) {
-        float money;
-        int dollars = 20;
+        final int MAX = 6;
 
-        System.out.println("Variable `dollars` is " + dollars);
-        money = dollars;
-
-        System.out.println("Variable `dollars` is still " + dollars +
-            ", but `money` is now " + money);
-   }
+        System.out.printf("%s%n", "Generate a pseudo-random number between 1–6:");
+        for(int i = 0; i < MAX; i++) {
+            /*
+             * 1. Generate a random double between 0.0 (inclusive)–1.0 (exclusive)
+             * 2. Multiply the result by MAX (6, in this case), meaning that random
+             *    numbers between 0–5 will be generated.
+             * 3. Cast the result to an int, removing the floating-point value to the
+             *    right of the period.
+             * 4. Add 1, since random doesn’t generate 1.0 in its generation of
+             *    pseudo-random numbers.
+             */
+            System.out.printf("%d%n", (int)(Math.random() * MAX) + 1);
+        }
+    }
 }
