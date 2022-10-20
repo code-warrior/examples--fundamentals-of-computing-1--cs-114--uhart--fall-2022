ad4b0db436230aad590a24af554148c4517ec33c
Roy A Vanegas <roy@thecodeeducators.com>
2022-10-20 10:34:52 -0400

    Update the PromotionConversion example

--- in-class-examples/math/conversions/PromotionConversion.java
+++ in-class-examples/math/conversions/PromotionConversion.java
@@ -1,11 +1,20 @@
+/*
+ * Conversion via promotion occurs automatically when certain operators need to
+ * modify their operands in order to perform the operation.
+ */
 public class PromotionConversion {
     public static void main(String[] args) {
-        float result;
-        float sum = 7.f;
+        float sum = 7; // or 7.f or 7.0f
         int count = 3;
 
-        result = sum / count;
-
-        System.out.println(result);
-   }
+        /*
+         * 1. The value to variable sum is really 7.0, not just 7, per the assignment
+         *    on line X, because it’s a float.
+         * 2. If the division had truly been 7/3, then integer division would
+         *    truncate the result, yielding a value of 2.
+         * 3. Thus, variable count is promoted to 3.0, so the final division is
+         *    7.0/3.0.
+         */
+        System.out.println(sum / count);
+    }
 }
