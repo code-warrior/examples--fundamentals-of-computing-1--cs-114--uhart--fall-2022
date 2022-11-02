6179ed7b4e21e196a1c6ded32a04280b00a84a47
Roy A Vanegas <roy@thecodeeducators.com>
2022-11-02 08:18:28 -0400

    Update BasicArray.java

--- textbook-examples/chapter-8-examples/BasicArray/src/BasicArray.java
+++ textbook-examples/chapter-8-examples/BasicArray/src/BasicArray.java
@@ -1,19 +1,23 @@
-//********************************************************************
+// **********************************************************************************
 //  BasicArray.java       Author: Lewis/Loftus
 //
 //  Demonstrates basic array declaration and use.
-//********************************************************************
+// **********************************************************************************
 
-public class BasicArray
-{
-    //-----------------------------------------------------------------
-    //  Creates an array, fills it with various integer values,
-    //  modifies one value, then prints them out.
-    //-----------------------------------------------------------------
-    public static void main(String[] args)
-    {
-        final int LIMIT = 15, MULTIPLE = 10;
+public class BasicArray {
+    // ------------------------------------------------------------------------------
+    //  Creates an array, fills it with various integer values, modifies one value,
+    //  then prints them out.
+    // ------------------------------------------------------------------------------
+    public static void main(String[] args) {
+        final int LIMIT = 15,
+            MULTIPLE = 10;
 
+        /*
+         * This could also have been written as, int list[] = new int[LIMIT],
+         * similar to main(String[] args) or main(String args[]). This is called
+         * alternate array syntax.
+         */
         int[] list = new int[LIMIT];
 
         //  Initialize the array values
@@ -22,7 +26,7 @@ public class BasicArray
 
         list[5] = 999;  // change one array value
 
-        //  Print the array values
+        //  Print the array values using an enhanced for
         for (int value : list)
             System.out.print(value + "  ");
     }
