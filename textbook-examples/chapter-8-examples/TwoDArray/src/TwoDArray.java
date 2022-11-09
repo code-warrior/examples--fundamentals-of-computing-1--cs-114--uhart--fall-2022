923fd28ced7a551d66b906c5f8ac7a3cf85787de
Roy Vanegas <roy@thecodeeducators.com>
2022-11-09 08:11:56 -0500

    Surround assignment operator with single spaces

--- textbook-examples/chapter-8-examples/TwoDArray/src/TwoDArray.java
+++ textbook-examples/chapter-8-examples/TwoDArray/src/TwoDArray.java
@@ -13,16 +13,16 @@ public class TwoDArray {
         int[][] table = new int[5][10];
 
         // Load the table with values
-        for (int row=0; row < table.length; row++) {
-            for (int col=0; col < table[row].length; col++) {
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
                 table[row][col] = row * 10 + col;
             }
         }
 
 
         // Print the table
-        for (int row=0; row < table.length; row++) {
-            for (int col=0; col < table[row].length; col++) {
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
                 System.out.print (table[row][col] + "\t");
             }
 
